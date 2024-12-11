13b69c1aef3f7b622f851981db95c21aa772b580
Ulysses Butler <git@uabutler.com>
2024-12-11 00:36:45 -0500

    Added module node abstractions

--- src/main/kotlin/com/uabutler/Compile.kt
+++ src/main/kotlin/com/uabutler/Compile.kt
@@ -1,6 +1,10 @@
 package com.uabutler
 
 import com.uabutler.ast.ProgramNode
+import com.uabutler.module.ModuleNodeRecordInterface
+import com.uabutler.module.ModuleNodeTranslatableInterface
+import com.uabutler.module.ModuleNodeVectorInterface
+import com.uabutler.module.ModuleNodeWireInterface
 import com.uabutler.references.IdentifierMap
 import com.uabutler.references.Scope
 import com.uabutler.references.ScopeValidator
@@ -18,4 +22,24 @@ fun compile(args: Array<String>) {
     ast.interfaces.forEach { println(it) }
 }
 
-fun main(args: Array<String>) = compile(args)
+fun test() {
+    val a = ModuleNodeRecordInterface(
+        mapOf(
+            "a1" to ModuleNodeWireInterface(),
+            "a2" to ModuleNodeWireInterface(),
+        )
+    )
+    val test = ModuleNodeRecordInterface(
+        mapOf(
+            "b1" to a,
+            "b2" to ModuleNodeVectorInterface(
+                listOf(a, a, a, a),
+            )
+        )
+    )
+
+    println(ModuleNodeTranslatableInterface.fromModuleNodeInterface(test))
+}
+
+// fun main(args: Array<String>) = compile(args)
+fun main(args: Array<String>) = test()
---
+++ src/main/kotlin/com/uabutler/module/ModuleNode.kt
@@ -0,0 +1,170 @@
+package com.uabutler.module
+
+sealed class ModuleNodeInterface {
+    abstract fun hasInput(): Boolean
+    abstract fun getInput(): ModuleNodeInterface?
+    abstract fun isCompatible(other: ModuleNodeInterface): Boolean
+
+    fun setInput(input: ModuleNodeInterface) {
+        when (input) {
+            is ModuleNodeWireInterface -> (this as ModuleNodeWireInterface).setInput(input)
+            is ModuleNodeVectorInterface -> (this as ModuleNodeVectorInterface).setInput(input)
+            is ModuleNodeRecordInterface -> (this as ModuleNodeRecordInterface).setInput(input)
+        }
+    }
+}
+
+class ModuleNodeWireInterface : ModuleNodeInterface() {
+    var inputNode: ModuleNodeWireInterface? = null
+
+    override fun hasInput() = inputNode != null
+    override fun getInput() = inputNode
+    override fun isCompatible(other: ModuleNodeInterface): Boolean {
+        return other is ModuleNodeWireInterface
+    }
+
+    fun setInput(input: ModuleNodeWireInterface) { inputNode = input }
+}
+
+sealed class ModuleNodeComplexInterface: ModuleNodeInterface()
+
+data class ModuleNodeVectorInterface(
+    val subInterfaces: List<ModuleNodeInterface>,
+): ModuleNodeComplexInterface() {
+    var inputNode: ModuleNodeVectorInterface? = null
+
+    override fun hasInput() = inputNode != null || subInterfaces.all { it.hasInput() }
+    override fun getInput() = inputNode
+    override fun isCompatible(other: ModuleNodeInterface): Boolean {
+        if (other !is ModuleNodeVectorInterface) return false
+        if (other.subInterfaces.size != subInterfaces.size) return false
+
+        return other.subInterfaces
+            .zip(subInterfaces)
+            .all { (subInterface, otherSubInterface) ->
+                subInterface.isCompatible(otherSubInterface)
+            }
+    }
+
+    fun setInput(input: ModuleNodeVectorInterface) {
+        if (inputNode != null || subInterfaces.any { it.hasInput() }) throw Exception("Interface already has input")
+
+        inputNode = input
+    }
+
+    fun setInput(index: Int, input: ModuleNodeInterface) {
+        if (inputNode != null || subInterfaces[index].hasInput()) throw Exception("Interface already has input")
+
+        subInterfaces[index].setInput(input)
+    }
+
+    fun setInput(startIndex: Int, input: ModuleNodeVectorInterface) {
+        if (inputNode != null) throw Exception("Interface already has input")
+
+        input.subInterfaces.forEachIndexed { i, it ->
+            if (subInterfaces[startIndex + i].hasInput()) throw Exception("Interface already has input")
+
+            subInterfaces[i].setInput(it)
+        }
+    }
+}
+
+data class ModuleNodeRecordInterface(
+    val subInterfaces: Map<String, ModuleNodeInterface>,
+): ModuleNodeComplexInterface() {
+    var inputNode: ModuleNodeRecordInterface? = null
+
+    override fun hasInput() = inputNode != null || subInterfaces.values.all { it.hasInput() }
+    override fun getInput() = inputNode
+    override fun isCompatible(other: ModuleNodeInterface): Boolean {
+        if (other !is ModuleNodeRecordInterface) return false
+        if (other.subInterfaces.size != subInterfaces.size) return false
+
+        // Not comparing the keys is deliberate
+        return other.subInterfaces.values
+            .zip(subInterfaces.values)
+            .all { (subInterface, otherSubInterface) ->
+                subInterface.isCompatible(otherSubInterface)
+            }
+    }
+
+    fun setInput(input: ModuleNodeRecordInterface) {
+        if (inputNode != null || subInterfaces.values.any { it.hasInput() }) throw Exception("Interface already has input")
+
+        inputNode = input
+    }
+
+    fun setInput(key: String, input: ModuleNodeRecordInterface) {
+        if (inputNode != null || subInterfaces[key]!!.hasInput()) throw Exception("Interface already has input")
+
+        subInterfaces[key]!!.setInput(input)
+    }
+}
+
+data class ModuleNodeTranslatableInterface private constructor(
+    val subInterfaces: Map<String, List<ModuleNodeWireInterface>>,
+) {
+    companion object {
+        fun fromModuleNodeInterface(moduleNodeInterface: ModuleNodeInterface): ModuleNodeTranslatableInterface {
+            return when (moduleNodeInterface) {
+                is ModuleNodeWireInterface -> {
+                    // I.e., interface { wire[1]; }
+                    ModuleNodeTranslatableInterface(mapOf("_wire" to listOf(moduleNodeInterface)))
+                }
+                is ModuleNodeRecordInterface -> {
+                    ModuleNodeTranslatableInterface(
+                        moduleNodeInterface.subInterfaces
+                            .map { port ->
+                                fromModuleNodeInterface(port.value).subInterfaces.mapKeys { "${port.key}_${it.key}" }
+                            }
+                            .fold(mutableMapOf()) { acc, map ->
+                                acc.putAll(map)
+                                acc
+                            }
+                    )
+                }
+                is ModuleNodeVectorInterface -> {
+                    when {
+                        moduleNodeInterface.subInterfaces.all { it is ModuleNodeWireInterface } -> {
+                            // Type erasure jumps in to ruin my life, yet again.
+                            val subInterfaces = moduleNodeInterface.subInterfaces
+                                .filterIsInstance<ModuleNodeWireInterface>()
+
+                            ModuleNodeTranslatableInterface(mapOf("_vector" to subInterfaces))
+                        }
+                        moduleNodeInterface.subInterfaces.all { it is ModuleNodeVectorInterface } -> {
+                            // Fuck type erasure
+                            val subInterfaces = moduleNodeInterface.subInterfaces
+                                .filterIsInstance<ModuleNodeVectorInterface>()
+                                .flatMap { it.subInterfaces }
+
+                            fromModuleNodeInterface(ModuleNodeVectorInterface(subInterfaces))
+                        }
+                        moduleNodeInterface.subInterfaces.all { it is ModuleNodeRecordInterface } -> {
+                            val portNames = moduleNodeInterface.subInterfaces
+                                .filterIsInstance<ModuleNodeRecordInterface>()
+                                .first()
+                                .subInterfaces
+                                .keys
+
+                            val subInterfaces = portNames.associate { key ->
+                                key to ModuleNodeVectorInterface(
+                                    moduleNodeInterface.subInterfaces
+                                        .filterIsInstance<ModuleNodeRecordInterface>()
+                                        .map { it.subInterfaces[key]!! }
+                                )
+                            }
+
+                            fromModuleNodeInterface(ModuleNodeRecordInterface(subInterfaces))
+                        }
+                        else -> throw Exception("Invalid subtype for module node vector interface")
+                    }
+                }
+            }
+        }
+    }
+
+    override fun toString(): String {
+        return "{" + subInterfaces.map { "${it.key}: wire[${it.value.size}];" }.joinToString(" ") + "}"
+    }
+}
