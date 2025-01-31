96320e56fe7ee3744bc1a9a2b6623f0c6f74292d
Ulysses Butler <git@uabutler.com>
2025-01-30 19:42:38 -0500

    Verilog IR: Created Verilog serializer

---
+++ src/main/kotlin/com/uabutler/v2/verilogir/module/statement/always/AlwaysStatement.kt
@@ -0,0 +1,50 @@
+package com.uabutler.v2.verilogir.module.statement.always
+
+import com.uabutler.v2.verilogir.VerilogSerialize
+import com.uabutler.v2.verilogir.module.statement.expression.Expression
+
+sealed class AlwaysStatement: VerilogSerialize
+
+data class BlockingAssignment(
+    val variableName: String,
+    val expression: Expression,
+): AlwaysStatement() {
+    override fun verilogSerialize() = "$variableName = ${expression.verilogSerialize()};"
+}
+
+data class NonBlockingAssignment(
+    val variableName: String,
+    val expression: Expression,
+): AlwaysStatement() {
+    override fun verilogSerialize() = "$variableName <= ${expression.verilogSerialize()};"
+}
+
+data class IfBranch(
+    val condition: Expression,
+    val then: List<AlwaysStatement>,
+)
+
+data class IfStatement(
+    val ifBranch: IfBranch,
+    val elseIfBranches: List<IfBranch>,
+    val elseStatements: List<AlwaysStatement>,
+): AlwaysStatement() {
+    override fun verilogSerialize() = buildString {
+        appendLine("if (${ifBranch.condition.verilogSerialize()}) begin")
+        val ifStatements = buildString { ifBranch.then.forEach { appendLine(it.verilogSerialize()) } }
+        appendLine(ifStatements.prependIndent())
+
+        val elseIfBranches = elseIfBranches.forEach { elseIfBranch ->
+            appendLine("end else if (${elseIfBranch.condition.verilogSerialize()}) begin")
+            val statements = buildString { elseIfBranch.then.forEach { appendLine(it.verilogSerialize()) } }
+            appendLine(statements.prependIndent())
+        }
+        appendLine(elseIfBranches)
+
+        appendLine("end else begin")
+        val elseStatements = buildString { elseStatements.forEach { appendLine(it.verilogSerialize()) } }
+        appendLine(elseStatements.prependIndent())
+
+        appendLine("end")
+    }
+}
