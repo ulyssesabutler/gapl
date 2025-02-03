2b886a6d612de6e97ed4f4f42083528215bbd9b6
Ulysses Butler <git@uabutler.com>
2025-02-03 00:51:39 -0500

    Verilog IR: Started work on builder, we have a scaffolding, just need to fill in the module body

--- src/main/kotlin/com/uabutler/v2/verilogir/module/Module.kt
+++ src/main/kotlin/com/uabutler/v2/verilogir/module/Module.kt
@@ -29,13 +29,21 @@ data class Module(
     }
 }
 
+enum class ModuleIODirection(val verilog: String) {
+    INPUT("input"),
+    OUTPUT("output"),
+}
+
 data class ModuleIO(
     val name: String,
+    val direction: ModuleIODirection,
     val type: DataType,
     val startIndex: Int,
     val endIndex: Int,
 ): VerilogSerialize {
     override fun verilogSerialize() = buildString {
+        append(direction.verilog)
+        append(" ")
         append(type.verilog)
         append(" [$startIndex:$endIndex] ")
         append(name)
