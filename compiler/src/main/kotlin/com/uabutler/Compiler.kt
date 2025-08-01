becbe72f7af328dc63573f9a1f728f572459c628
Ulysses Butler <git@uabutler.com>
2025-08-01 17:11:56 -0400

    Added pass to remove all PassThrough Nodes, simplify the AST a bit

--- compiler/src/main/kotlin/com/uabutler/Compiler.kt
+++ compiler/src/main/kotlin/com/uabutler/Compiler.kt
@@ -1,13 +1,15 @@
 package com.uabutler
 
 import com.uabutler.netlistir.builder.ModuleBuilder
+import com.uabutler.netlistir.transformer.Transformer
 import com.uabutler.verilogir.builder.VerilogBuilder
 
 object Compiler {
     fun compile(gapl: String): String {
         val ast = Parser.fromString(gapl).program()
         val netlistModules = ModuleBuilder(ast).buildAllModules()
-        val verilogirModules = netlistModules.map { VerilogBuilder.verilogModuleFromGAPLModule(it) }
+        val transformedModules = Transformer.allTransformations(netlistModules)
+        val verilogirModules = transformedModules.map { VerilogBuilder.verilogModuleFromGAPLModule(it) }
 
         return verilogirModules.joinToString("\n") { it.verilogSerialize() }
     }
---
+++ compiler/src/main/kotlin/com/uabutler/netlistir/transformer/PassThroughRemover.kt
@@ -0,0 +1,51 @@
+package com.uabutler.netlistir.transformer
+
+import com.uabutler.netlistir.netlist.Module
+import com.uabutler.netlistir.netlist.Node
+import com.uabutler.netlistir.netlist.PassThroughNode
+
+object PassThroughRemover: Transformer {
+    override fun transform(original: List<Module>): List<Module> {
+        return original.map { transformModule(it) }
+    }
+
+    private fun transformModule(module: Module): Module {
+        val passThroughNodes = module.getBodyNodes().filterIsInstance<PassThroughNode>()
+
+        passThroughNodes.forEach {
+            disconnectPassThroughNode(it)
+            module.removeNode(it)
+        }
+
+        return module
+    }
+
+    private fun disconnectPassThroughNode(node: Node) {
+        val module = node.parentModule
+
+        val passThroughWirePairs = node.inputWires().zip(node.outputWires())
+
+        /* Original Source -> PassThrough Input -> PassThrough Output ---> Original Sink 1
+         *                                                             \-> Original Sink 2
+         *
+         * PassThrough wire pairs represent the pair (PassThrough Input, PassThrough Output)
+         * For each connection from the PassThrough Output to a given Original Sink, we
+         *   1. Disconnect that Original Sink from the PassThrough Output
+         *   2. Connect that Original Sink to the Original Source
+         * Once all the Original Sinks are now connected to the Original Sources, we can also
+         * remove the connection of the PassThrough Input to the Original Source.
+         */
+        passThroughWirePairs.forEach { (input, output) ->
+            val originalSource = module.getConnectionForInputWire(input).outputWire
+
+            val originalSinkConnection = module.getConnectionsForOutputWire(output)
+
+            originalSinkConnection.forEach { connection ->
+                module.disconnect(connection.inputWire)
+                module.connect(connection.inputWire, originalSource)
+            }
+
+            module.disconnect(input)
+        }
+    }
+}
